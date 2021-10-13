efd768b05acbc5ef98b05c6941741f4e5e8f7ab0
alexandre.jottard <alexandre.jottard@gmail.com>
2021-10-13 23:05:58 +0200

    feat: add a model

---
+++ RazorMVCTutorial/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorMVCTutorial.Models;
+
+namespace RazorMVCTutorial.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorMVCTutorial.Models.Movie> Movie { get; set; }
+    }
+}
